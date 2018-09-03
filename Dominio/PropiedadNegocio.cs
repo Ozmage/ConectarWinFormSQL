187fa2304e60e7b7ec5f17fb9ea3376d25328489
Diego Leiva <diegoleiva-86@hotmail.com>
2018-09-02 23:12:14 -0300

    Agregar archivos de proyecto.

---
+++ Dominio/PropiedadNegocio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Dominio
+{
+    class PropiedadNegocio
+    {
+
+        public IList<>
+            //conexion
+            SqlConnection onexion = new SqlConnection;
+
+
+
+        //sentencia
+
+        //objeto recepcion
+
+        try 
+	{	        
+		
+	}
+	catch (global::System.Exception)
+	{
+
+		throw;
+	}
+
+    }
+}
